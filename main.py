05e90f259fa0fd8c6a34630aa038b0cd767e6659
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-30 19:13:24 +0700

    use custome code

--- main.py
+++ main.py
@@ -1,98 +1,118 @@
+# import pygame libraries!
 import pygame
 
+# initialize the pygame
 pygame.init()
 
+# definen the global variables
+RED = (255,50,50)
+WHITE = (255,255,255)
+GREEN = (0,128,0)
 WIDTH = 500
 HEIGHT = 500
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("pesawat2an")
 
-MERAH = (250, 50, 50)
-PUTIH = (255, 255, 255)
-HIJAU = (50, 250, 50)
+# define the screen size
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("First Game")
 
-class Peluru:
-  def __init__(self, x, y):
-    self.size = 50
-    self.x = x
-    self.y = y
-    self.langkah = 5
-  
-  def tampilkan(self):
-    pygame.draw.rect(screen, MERAH, (self.x, self.y, self.size, self.size))
-  
-  def gerak(self):
-    self.y = self.y - self.langkah
+# define the plane object
+class Plane:
 
-  def keatas(self):
-    if self.y < 0:
-      return True
-  
+    def __init__(self):
+        self.width = 70
+        self.height = 59.5
+        # self.size = 50
+        self.x = WIDTH / 2
+        self.y = HEIGHT - self.height
+        self.turn_left = False
+        self.turn_right = False
+        self.move_range = 10 # px
 
+    def show(self):
+        plane_img = pygame.image.load("assets\\plane.png")
+        plane_img = pygame.transform.scale(plane_img, (self.width, self.height))
+        screen.blit(plane_img, (self.x, self.y))
 
-class Pesawat:
-  def __init__(self):
-    self.size = 50
-    self.x = WIDTH / 2
-    self.y = HEIGHT - self.size
-    self.tombolKiri = False
-    self.tombolKanan = False
-    self.langkah = 3
-  
-  def tampilkan(self):
-    gambar = pygame.image.load("player.png")
-    gambar = pygame.transform.scale(gambar, (self.size, self.size))
-    screen.blit(gambar, (self.x, self.y))
-    # pygame.draw.rect(screen, HIJAU, (self.x, self.y, self.size, self.size))
+    def move(self):
+        if self.turn_left:
+            self.x -= self.move_range
+        if self.turn_right:
+            self.x += self.move_range
 
-  def gerak(self):
-    if self.tombolKanan:
-      self.x = self.x + self.langkah
-    if self.tombolKiri:
-      self.x = self.x - self.langkah
+# define the bullet object
+class Bullet:
+    
+    out_screen = False
 
+    def __init__(self, x, y):
+        self.width = 20
+        self.height = 60
+        self.x = x - (self.width/2)
+        self.y = y
+        self.move_range = 5
 
+    def show(self):
+        bullet_img = pygame.image.load("assets\\fire-bullet.png")
+        bullet_img = pygame.transform.scale(bullet_img, (self.width, self.height))
+        screen.blit(bullet_img, (self.x, self.y))
 
+    def move(self):
+        self.y -= self.move_range
+        if self.y < -1 * self.height:
+            self.out_screen = True
 
+# define the main function
 def main():
-  run = True
-  while run:
-    for e in pygame.event.get():
-      if e.type == pygame.QUIT:
-        run = False
-      
-      # KEYDOWN
-      if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-        run = False
-      elif e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
-        pesawat.tombolKanan = True
-      elif e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
-        pesawat.tombolKiri = True
-      elif e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
-        bullets.append(Peluru(pesawat.x, pesawat.y))
+    
+    # set the running variable
+    running = True
+    
+    # run the pygame in while loop
+    while running:
 
-      # KEYUP
-      if e.type == pygame.KEYUP:
-        pesawat.tombolKanan = False
-        pesawat.tombolKiri = False
-      
-    screen.fill(PUTIH)
+        pygame.time.delay(10)
+        
+        # retrived events
+        for e in pygame.event.get():
 
-    # PELULU
-    for bullet in bullets:
-      bullet.tampilkan()
-      bullet.gerak()
-      if bullet.keatas():
-        bullets.remove(bullet)
+            # close the window if close [x] button being triggered
+            if e.type == pygame.QUIT:
+                running = False
+            elif e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                running = False
 
-    # PESAWAT
-    pesawat.tampilkan()
-    pesawat.gerak()
+            # keydown config
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_a:
+                plane.turn_left = True
+            elif e.type == pygame.KEYDOWN and e.key == pygame.K_s:
+                plane.turn_right = True
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
+                bullets.append(Bullet(plane.x+(plane.width/2), plane.y))
 
-    pygame.display.update()
+            # keyup config
+            if e.type == pygame.KEYUP:
+                plane.turn_left = False
+                plane.turn_right = False
 
+        # set the screen color
+        screen.fill(WHITE)
+
+        # bullets obj
+        for bullet in bullets:
+            bullet.show()
+            bullet.move()
+            if bullet.out_screen:
+                bullets.remove(bullet)
+
+        # plane obj
+        plane.show()
+        plane.move()
+        
+        # update the screen for any changes
+        pygame.display.update()
 
 if __name__ == "__main__":
-  pesawat = Pesawat()
-  bullets = []
-  main()
+
+    plane = Plane()
+    bullets = []
+    main()
